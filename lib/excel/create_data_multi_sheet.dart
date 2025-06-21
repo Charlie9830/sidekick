3ac8dc08509ab3b7ceaadf6574187cb9c2ded83c
Charlie9830 <charliewjhall@gmail.com>
2025-06-21 14:12:31 +1000

    Disabled Smart Sneak Assignment

--- lib/excel/create_data_multi_sheet.dart
+++ lib/excel/create_data_multi_sheet.dart
@@ -31,83 +31,94 @@ void createDataMultiSheet({
   final cablesByParentMultiId =
       cables.values.groupListsBy((cable) => cable.parentMultiId);
 
-  final Map<String, DataMultiModel?> assignedSneakPanelSlots = {
-    'Red': null,
-    'White': null,
-    'Blue': null,
-    'Orange': null,
-    'Yellow': null,
-    'Green': null,
-    'Purple': null,
-    'Brown': null,
-    'Red/Grey': null,
-    'White/Grey': null,
-    'Blue/Grey': null,
-    'Orange/Grey': null,
-    'Yellow/Grey': null,
-    'Green/Grey': null,
-    'Purple/Grey': null,
-    'Brown/Grey': null,
-  };
 
-  // Try to Assign Multis to their slots on the Sneak panel by Color in a first pass best effort manner. We will iterate once more
-  // after this to ensure all remaining multis get assigned somewhere.
-  final List<DataMultiModel> rejects = [];
-  for (final multi in dataMultis.values) {
-    final colorName = locations[multi.locationId]?.color.name;
+  // TODO: Rethink this.. It borks when you have more then 1 Data rack because it ends up assigning Node outputs on the first rack to Sneak Panel slots
+  // on the second rack.. Should make it a bit smarter and and then possibly an option to turn on and off.
 
-    // Could'nt get a Suitable color. Send to the Reject Pile.
-    if (colorName == null || colorName == const LabelColorModel.none().name) {
-      rejects.add(multi);
-      continue;
-    }
+  // // Try to Assign Multis to their slots on the Sneak panel by Color in a first pass best effort manner. We will iterate once more
+  // // after this to ensure all remaining multis get assigned somewhere.
 
-    // Assign based on exact color if available.
-    if (assignedSneakPanelSlots.containsKey(colorName) &&
-        assignedSneakPanelSlots[colorName] == null) {
-      assignedSneakPanelSlots[colorName] = multi;
-      continue;
-    }
+    // final Map<String, DataMultiModel?> assignedSneakPanelSlots = {
+  //   'Red': null,
+  //   'White': null,
+  //   'Blue': null,
+  //   'Orange': null,
+  //   'Yellow': null,
+  //   'Green': null,
+  //   'Purple': null,
+  //   'Brown': null,
+  //   'Red/Grey': null,
+  //   'White/Grey': null,
+  //   'Blue/Grey': null,
+  //   'Orange/Grey': null,
+  //   'Yellow/Grey': null,
+  //   'Green/Grey': null,
+  //   'Purple/Grey': null,
+  //   'Brown/Grey': null,
+  // };
 
-    // Assign based on Secondary (Greyed) color if available.
-    if (assignedSneakPanelSlots.containsKey('$colorName/Grey') &&
-        assignedSneakPanelSlots[colorName] == null) {
-      assignedSneakPanelSlots['$colorName/Grey'] = multi;
-      continue;
-    }
 
-    // Nothing available. Send to the Reject Pile for now.
-    rejects.add(multi);
-  }
 
-  // Now assign the rejects to any leftover slots. Only creating more once we have completly filled all available slots.
-  for (final rejectedMulti in rejects) {
-    final nextAvailableSlotKey = assignedSneakPanelSlots.entries
-        .firstWhereOrNull((entry) => entry.value == null)
-        ?.key;
 
-    if (nextAvailableSlotKey != null) {
-      assignedSneakPanelSlots[nextAvailableSlotKey] = rejectedMulti;
-      continue;
-    }
+  // final List<DataMultiModel> rejects = [];
+  // for (final multi in dataMultis.values) {
+  //   final colorName = locations[multi.locationId]?.color.name;
 
-    assignedSneakPanelSlots[rejectedMulti.uid] = rejectedMulti;
-  }
+  //   // Could'nt get a Suitable color. Send to the Reject Pile.
+  //   if (colorName == null || colorName == const LabelColorModel.none().name) {
+  //     rejects.add(multi);
+  //     continue;
+  //   }
 
-  for (final (index, multi) in assignedSneakPanelSlots.values.indexed) {
-    if (multi == null) {
-      // Append an Empty Row.
-      sheet.appendRow([
-        IntCellValue(index + 1),
-        TextCellValue(''),
-        TextCellValue(''),
-        TextCellValue(''),
-        TextCellValue(''),
-        TextCellValue(''),
-        TextCellValue(''),
-      ]);
-      continue;
-    }
+  //   // Assign based on exact color if available.
+  //   if (assignedSneakPanelSlots.containsKey(colorName) &&
+  //       assignedSneakPanelSlots[colorName] == null) {
+  //     assignedSneakPanelSlots[colorName] = multi;
+  //     continue;
+  //   }
+
+  //   // Assign based on Secondary (Greyed) color if available.
+  //   if (assignedSneakPanelSlots.containsKey('$colorName/Grey') &&
+  //       assignedSneakPanelSlots[colorName] == null) {
+  //     assignedSneakPanelSlots['$colorName/Grey'] = multi;
+  //     continue;
+  //   }
+
+  //   // Nothing available. Send to the Reject Pile for now.
+  //   rejects.add(multi);
+  // }
+
+  // // Now assign the rejects to any leftover slots. Only creating more once we have completly filled all available slots.
+  // for (final rejectedMulti in rejects) {
+  //   final nextAvailableSlotKey = assignedSneakPanelSlots.entries
+  //       .firstWhereOrNull((entry) => entry.value == null)
+  //       ?.key;
+
+  //   if (nextAvailableSlotKey != null) {
+  //     assignedSneakPanelSlots[nextAvailableSlotKey] = rejectedMulti;
+  //     continue;
+  //   }
+
+  //   assignedSneakPanelSlots[rejectedMulti.uid] = rejectedMulti;
+  // }
+
+  for (final (index, multi) in dataMultis.values.indexed) {
+    // TODO
+    // Disabled until Smarter Sneak Slot assignment is fixed.
+    //
+    // if (multi == null) {
+    //   // Append an Empty Row.
+    //   sheet.appendRow([
+    //     IntCellValue(index + 1),
+    //     TextCellValue(''),
+    //     TextCellValue(''),
+    //     TextCellValue(''),
+    //     TextCellValue(''),
+    //     TextCellValue(''),
+    //     TextCellValue(''),
+    //   ]);
+    //   continue;
+    // }
 
     final locationColor = locations[multi.locationId]?.color;
 
